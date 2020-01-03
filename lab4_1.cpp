b09a4b04dbb02a6174c67a600a8d1d8c9e34af50
mek <themakza0021@gmail.com>
2020-01-03 14:53:28 +0700

    ver1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main()
+{
+    double x,y;
+    double e=2.71828;
+    cout << "Enter x = ";
+    cin >> x;
+    y=(3*pow(x,3))+(2*e+pow(2,(2*x)+1))+(sqrt(pow(x,2)+1));
+    cout << "Result y = " << y;
+    return 0;
+}
+    
